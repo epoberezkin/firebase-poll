29d5c465804af43b7749314df00c3878295f1399
Evgeny Poberezkin <e.poberezkin@me.com>
2016-03-27 17:06:33 +0100

    first commit

---
+++ answers.js
@@ -0,0 +1,26 @@
+(function() {
+'use strict';
+
+var templateRow = document.getElementById('template');
+var tableBody = document.getElementById('tableBody');
+
+var dataRef = new Firebase('https://incandescent-fire-6958.firebaseio.com/answers');
+dataRef.on('child_added', function (snapshot) {
+    var item = snapshot.val();
+    console.log(item);
+    addRow(item);
+});
+
+
+function addRow(item) {
+    var row = templateRow.cloneNode(true);
+    row.style.display = '';
+    row.removeAttribute('id');
+    for (var prop in item) {
+        var field = row.querySelector('[data=' + prop + ']');
+        field.innerHTML = item[prop];
+    }
+    tableBody.appendChild(row);
+}
+
+})();
---
+++ index.js
@@ -0,0 +1,34 @@
+(function() {
+'use strict';
+
+var form = document.getElementById('questions');
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    var data = getData();
+    var dataRef = new Firebase('https://incandescent-fire-6958.firebaseio.com/answers');
+    dataRef.push(data, function() {
+        window.alert('Form submitted');
+    });
+};
+
+
+function getData() {
+    var data = {};
+    var inputs = form.querySelectorAll('input, textarea');
+    for (var i=0; i<inputs.length; i++) {
+        var el = inputs[i];
+        switch (el.type) {
+            case 'radio':
+                if (el.checked) data[el.name] = el.value;
+                break;
+            case 'text':
+            default:
+                data[el.name] = el.value;
+                break;
+        }
+    }
+    return data;
+}
+
+})();
